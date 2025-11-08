f5cb9c91c2f3aa1e647628cc8e77bfff1255053c
Vamsi Krishna Sivakavi <vamsikrishna7909@gmail.com>
2025-11-07 21:30:56 -0800

    AdKit binary package 1.0.0

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "AdKitBinary",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AdKitBinary",
-            targets: ["AdKitBinary"]),
+            name: "AdKit",
+            targets: ["AdKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -20,5 +21,6 @@ let package = Package(
             name: "AdKitBinaryTests",
             dependencies: ["AdKitBinary"]
         ),
+        .binaryTarget(name: "AdKit", url: "https://github.com/vamsikrishna7909/AdKit/releases/download/1.0.0/AdKit.xcframework.zip", checksum: "1be1d679b8dabf3d0358195106bee1306f0602aec9cdc3ef0f3496ea2053d9fc")
     ]
 )
